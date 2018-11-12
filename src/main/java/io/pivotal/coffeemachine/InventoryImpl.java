c0d36e41140b96a93a82ba147000bfdb33d6ea95
saptaparna <saptaparna@ccs.neu.edu>
2018-11-11 20:08:14 -0500

    Modified InventoryImpl to pass InventoryImplTests

--- src/main/java/io/pivotal/coffeemachine/InventoryImpl.java
+++ src/main/java/io/pivotal/coffeemachine/InventoryImpl.java
@@ -1,17 +1,28 @@
 package io.pivotal.coffeemachine;
 
-import java.util.Map;
+import java.util.*;
 
 public class InventoryImpl implements Inventory {
 
+	private Map<String, Integer> ingredients;
+
+	public InventoryImpl() {
+		ingredients = new HashMap<>();
+		ingredients.put("coffee", 10);
+		ingredients.put("cream", 10);
+		ingredients.put("sugar", 10);
+	}
+
 	public Map<String, Integer> getIngredients() {
-		// TODO Auto-generated method stub
-		return null;
+		return this.ingredients;
 	}
 
 	public void deduct(String name, Integer amount) {
-		// TODO Auto-generated method stub
-
+		if(ingredients.containsKey(name)) {
+			if(ingredients.get(name) >= amount) {
+				ingredients.put(name, ingredients.get(name)-amount);
+			}
+		}
 	}
 
 }
